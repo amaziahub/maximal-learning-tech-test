1d204e58ed68ab2115af32b9732da02bb05cefd3
Amazia Gur <amaziagur@Amazias-MacBook-Pro.local>
2025-02-24 16:18:18 +0000

    chore: add get_question_by_session_id

--- tech_test/session_service.py
+++ tech_test/session_service.py
@@ -1,6 +1,5 @@
 import uuid
-from time import time  # Correct the import for time
-
+from time import time
 from tech_test.quiz_service import QuizService
 
 
@@ -12,7 +11,6 @@ class SessionService:
     def init_session_for_user(self, user_id):
         session_id = str(uuid.uuid4())
 
-        # Get a question from the QuizService
         quiz_service = QuizService()
         question_id, question = quiz_service.get_question()
 
@@ -20,10 +18,15 @@ class SessionService:
             "user_id": user_id,
             "session_id": session_id,
             "question_id": question_id,
-            "question" : question,
-            "timestamp": time()  # Correct timestamp usage
+            "question": question,
+            "timestamp": time()
         }
 
         self.sessions[user_id] = session_data
 
         return session_data
+
+    def get_question_by_session_id(self, session_id):
+        session_data = self.sessions.get(session_id)
+        return session_data if session_data["question_id"] else None
+
